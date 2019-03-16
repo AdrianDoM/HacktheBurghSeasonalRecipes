ec19ec2e3dc3fa333622becaf1a4099972c2bc8f
Simon <kausi@heka.or.at>
2019-03-16 22:33:03 +0000

    port 80

--- server.py
+++ server.py
@@ -64,5 +64,5 @@ def helloworld():
 
 if __name__ == "__main__":
 	# app.run(port=5000)
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	#port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=80)
