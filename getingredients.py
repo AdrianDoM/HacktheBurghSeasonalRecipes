f86e0c35a2b142173157077b2206ab539ac19f32
AdrianDoM <drnaia100@gmail.com>
2019-03-16 23:41:46 +0000

    Rate by carbon

--- getingredients.py
+++ getingredients.py
@@ -1,5 +1,7 @@
 import csv
 from operator import itemgetter
+from carbon import carbon
+from categories import categories
 
 def getBest():
     columnNames = ['Ingredient{:02}'.format(i) for i in range(1, 20)]
@@ -16,14 +18,20 @@ def getBest():
         reader = csv.DictReader(csvfile)
         for idx, row in enumerate(reader):
             score = 0
+            footprint = 0
             for column in columnNames:
-                for ing, scr in ingredients:
-                    if ing in row[column]:
-                        score += scr
-            recipes.append((idx, row['Title'], score))
+                ingredient = row[column]
+                if ingredient:
+                    for ing, scr in ingredients:
+                        if ing in row[column]:
+                            score += scr
+                    cat = categories[ingredient]
+                    footprint += carbon[cat]
+            score -= footprint/6
+            recipes.append((idx, row['Title'], score, footprint))
 
     recipes.sort(key=(lambda x: x[2]), reverse=True)
     return recipes
 
-getBest()
+print(getBest())
 
