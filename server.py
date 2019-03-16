9beae212911964c7e9a1b33cc5f88d3ad8a4a62b
lorde cookie <33136297+WilliLam@users.noreply.github.com>
2019-03-16 16:04:32 +0000

    ww

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import pandas as pd
+import json
+recipes = pd.read_csv('recipes.csv', delimiter=',')
+seasonals = pd.read_csv('season.csv', delimiter=',')
+print(seasonals)
+print(recipes)
+#print(recipes)
+app = Flask(__name__)
+
+def getingredients(r):
+	pass
+
+@app.route('/')
+def index():
+	#return 'sanity check'
+	return render_template('test.html')
+
+def getrankings():
+	pass
+
+@app.route('/results', methods=['POST','GET'])
+def helloworld():
+	a = ['spaghetti','Tomato pie', '<a href=www.google.com>whut</a>']
+	#let a be list of recipes
+	print('bob')
+	return (render_template('results.html', recipes=json.dumps(a)))
+
+
+if __name__ == "__main__":
+	#pass
+	app.run()
