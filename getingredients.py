2e4ff3d8ce494adcdbbfe0710623a28f2055be1d
AdrianDoM <drnaia100@gmail.com>
2019-03-17 00:08:28 +0000

    For William

--- getingredients.py
+++ getingredients.py
@@ -3,9 +3,8 @@ from operator import itemgetter
 from carbon import carbon
 from categories import categories
 
-def getBest():
+def getBest(month, pref1=None, pref2=None):
     columnNames = ['Ingredient{:02}'.format(i) for i in range(1, 20)]
-    month = 'Jun'
     recipes = []
     ingredients = set()
 
@@ -20,11 +19,15 @@ def getBest():
             score = 0
             footprint = 0
             for column in columnNames:
-                ingredient = row[column]
+                ingredient = row[column].lower()
                 if ingredient:
                     for ing, scr in ingredients:
-                        if ing in row[column]:
+                        if ing in ingredient:
                             score += scr
+                    if pref1 and pref1 in ingredient:
+                        score += 3
+                    if pref2 and pref2 in ingredient:
+                        score += 3
                     cat = categories[ingredient]
                     footprint += carbon[cat]
             score -= footprint/6
@@ -33,5 +36,5 @@ def getBest():
     recipes.sort(key=(lambda x: x[2]), reverse=True)
     return recipes
 
-print(getBest())
+print(getBest('Oct'))
 
