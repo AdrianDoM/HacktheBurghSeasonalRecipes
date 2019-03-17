3d70f47f9ca2fea01b13e05ecbeb7c78e2e29c80
Simon <kausi@heka.or.at>
2019-03-17 05:21:47 +0000

    /recipes added for app (new)

--- server.py
+++ server.py
@@ -61,7 +61,7 @@ def get5recipes(month, pref1, pref2):
 	return (bestrecipes)
 
 @app.route('/recipes', methods=['GET', 'POST'])
-def helloworld():
+def recipes():
 	#for simon
 	# preferencesform = request.args
 	#before
